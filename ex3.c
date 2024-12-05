c68ab08f1a7996680651aa77e3e5071a6556e2ef
Shira Cababia <shiraca200@gmail.com>
2024-12-05 23:06:40 +0200

    Part 0 (Initializing the data structure) + Part 1 (Enter Daily Data For A Brand)

--- ex3.c
+++ ex3.c
@@ -1,5 +1,5 @@
 /******************
-Name:
+Name: Shira Cababia
 ID:
 Assignment: ex3
 *******************/
@@ -11,19 +11,79 @@ Assignment: ex3
 #define NUM_OF_TYPES 4
 #define TYPES_NAMES 10
 #define DAYS_IN_YEAR 365
-#define addOne  1
-#define addAll  2  
-#define stats  3
-#define print  4
-#define insights  5
-#define deltas  6
-#define done  7
+#define ADD_ONE 1
+#define ADD_ALL 2
+#define STATS 3
+#define PRINT 4
+#define INSIGHTS 5
+#define DELTAS 6
+#define DONE 7
+void printMenu();
+void initializeData(int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES]);                                         // 0
+void insertData(int arr[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES], int carBrandIndx, int type, int sumToInsert); // 1
 
 char brands[NUM_OF_BRANDS][BRANDS_NAMES] = {"Toyoga", "HyunNight", "Mazduh", "FolksVegan", "Key-Yuh"};
 char types[NUM_OF_TYPES][TYPES_NAMES] = {"SUV", "Sedan", "Coupe", "GT"};
 
+int main()
+{
+    int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES];
+    int days[NUM_OF_BRANDS] = {0};
+    initializeData(cube);
+    int choice;
+    printMenu();
+    scanf("%d", &choice);
+    while (choice != DONE)
+    {
+        switch (choice)
+        {
+        case ADD_ONE: // 1
+        {
+            int arr[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES];
+            initializeData(arr);
+            int carBrandIndx, sumFirst, sumSecond, sumThird, sumFourth;
+            printf("Entert a car brand index and a series of 4 integers, each representing the daily sales sum for each car type\n");
+            scanf(" %d %d %d %d %d", carBrandIndx, sumFirst, sumSecond, sumThird, sumFourth);
+            if (carBrandIndx < 0 || carBrandIndx > 4)
+            {
+                printf("This brand is not valid\n");
+                printMenu();
+                break;
+            }
+            insertData(arr, carBrandIndx, 0, sumFirst);
+            insertData(arr, carBrandIndx, 1, sumSecond);
+            insertData(arr, carBrandIndx, 2, sumThird);
+            insertData(arr, carBrandIndx, 3, sumFourth);
+            break;
+        }
 
-void printMenu(){
+        case ADD_ALL: // 2
+            //...
+            break;
+        case STATS: // 3
+            //...
+            break;
+        case PRINT: // 4
+            //...
+            break;
+        case INSIGHTS: // 5
+            //...
+            break;
+        case DELTAS: // 6
+            //...
+            break;
+        default:
+            printf("Invalid input\n");
+        }
+        printMenu();
+        scanf("%d", &choice);
+    }
+    printf("Goodbye!\n");
+    return 0;
+}
+
+void printMenu()
+{
     printf("Welcome to the Cars Data Cube! What would you like to do?\n"
            "1.Enter Daily Data For A Brand\n"
            "2.Populate A Day Of Sales For All Brands\n"
@@ -34,32 +94,29 @@ void printMenu(){
            "7.exit\n");
 }
 
-
-int main() {
-    int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES];
-    int days[NUM_OF_BRANDS] = {0};
-    int choice;
-    printMenu();
-    scanf("%d", &choice);
-    while(choice != done){
-        switch(choice){
-            case addOne:
-                //...
-                break;
-            case addAll:
-                //...
-                break;
-            /*
-             ......
-             */
-            default:
-                printf("Invalid input\n");
+void initializeData(int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES])
+{
+    for (int i = 0; i < DAYS_IN_YEAR; i++)
+    {
+        for (int j = 0; j < NUM_OF_BRANDS; j++)
+        {
+            for (int k = 0; k < NUM_OF_TYPES; k++)
+            {
+                cube[i][j][k] = -1;
+            }
         }
-        printMenu();
-        scanf("%d", &choice);
     }
-    printf("Goodbye!\n");
-    return 0;
 }
 
-
+void insertData(int arr[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES], int carBrandIndx, int type, int sumToInsert)
+{
+    int count = 0;
+    while (arr[count][carBrandIndx][type] != -1)
+    {
+        count++;
+    }
+    if (arr[count][carBrandIndx][type] == -1)
+    {
+        arr[count][carBrandIndx][type] = sumToInsert;
+    }
+}
